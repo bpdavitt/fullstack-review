3da57d67c137b71dd9cc2f2608b0c2a6577d5f03
Brendan <bpdavitt@gmail.com>
2019-06-28 13:00:22 -0500

    Server post takes an object with username key; queries github API and returns an array of repo objects. Next step is to add those repos to DB

--- server/index.js
+++ server/index.js
@@ -1,26 +1,42 @@
 const express = require('express');
-let app = express();
-let db = require('../database')
+const app = express();
+const db = require('../database')
+const helper = require('../helpers/github.js')
+
 
 app.use(express.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 
 app.post('/repos', function (req, res) {
-  console.log(req.body);
-  db.save(req.body, (err, info) => {
-    if (err) {
-      console.log('Error posting a repo');
-      res.status(400).send()
-    } else {
-      res.send('Request received')
-    }
-  })
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log(req.body);
+  helper.getReposByUsername(req.body.username, (err, repos) => {
+    if(err) {
+      console.log('Error in getting repos with username', err);
+      res.status(404).end()
+    } else {
+      console.log(typeof repos.body);
+      //Convert JSON string to array of objects
+      repos = JSON.parse(repos.body);
+      console.log(repos.length);
+      res.send('Check out these repos')
+    }
+  })
+
+  // db.save(req.body, (err, info) => {
+  //   if (err) {
+  //     console.log('Error posting a repo');
+  //     res.status(400).send()
+  //   } else {
+  //     res.send('Request received')
+  //   }
+  // })
 });
+
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
