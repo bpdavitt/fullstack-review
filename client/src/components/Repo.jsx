bd79c2bccb821e8be79c6ef1fb4120e4d184da06
Brendan <bpdavitt@gmail.com>
2019-06-28 16:18:00 -0500

    All pulled repos rendering on page

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 const Repo = (props) => {
 
+  const {repo} = props;
+
+  return (
+    <tr>
+      <th>{repo.name}</th>
+      <th>{repo.owner}</th>
+      <th>{repo.forks}</th>
+      <th>{repo.url}</th>
+    </tr>
+  )
 }
 
 export default Repo;
